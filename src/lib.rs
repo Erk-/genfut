9c36e230225d0f6b1cf82cd1bfefe77ed30fc33c
Valdemar Erk <valdemar@erk.io>
2020-04-09 16:54:54 +0200

    Make the library ready for release
    Signed-off-by: Valdemar Erk <valdemar@erk.io>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//!# Genfut
+//!
+//!This is a tool to generate a rust libary to interact with exported functions from a futhark file.
+//!
+//!## Usage
+//!
+//!### As an executable binary
+//!```shell
+//!genfut <Rust lib name> <futhark_file.fut>
+//!```
+//!
+//!### As a library
+//!
+//!`build.rs`
+//!```rust
+//!use genfut::genfut;
+//!
+//!fn main() {
+//!    genfut("<Rust lib name>", "futhark_file.fut")
+//!}
+//!
+//!```
+
+
 #![allow(unused_must_use)]
 #![allow(unused_variables)]
 
