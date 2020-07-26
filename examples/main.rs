5817f5474bc2abf9c841d6834dcb286b8111e289
Valdemar Erk <valdemar@erk.io>
2020-07-26 14:22:11 +0200

    Update main

--- examples/main.rs
+++ examples/main.rs
@@ -6,10 +6,10 @@ fn main() -> Result<(), Error> {
     let a = vec![1, 2, 3, 4];
     let b = vec![2, 3, 4, 1];
 
-    let mut ctx = FutharkContext::new();
+    let ctx = FutharkContext::new();
 
-    let a_arr = Array_i32_2d::from_vec(ctx, &a, &vec![2, 2])?;
-    let b_arr = Array_i32_2d::from_vec(ctx, &b, &vec![2, 2])?;
+    let a_arr = Array_i32_2d::from_vec(&ctx, &a, &vec![2, 2])?;
+    let b_arr = Array_i32_2d::from_vec(&ctx, &b, &vec![2, 2])?;
 
     let res_arr = ctx.matmul(a_arr, b_arr)?;
 
